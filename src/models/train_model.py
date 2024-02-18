f432e85a366be0eff5dc0f1907b943b900604c7f
Holden Babineaux <holden.bab@outlook.com>
2024-02-18 00:33:20 -0600

    Defines test/train split for feature-extracted data

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -20,14 +20,32 @@ plt.rcParams["lines.linewidth"] = 2
 # ------------------------------------------------------------ #
 DATA_PATH = Path("../../data")
 DATA_INTERIM_PATH = Path(DATA_PATH, "interim")
-DATA_PKL_FILENAME_OUTLIERS_REM = "01_75ms_outliers_removed_chauvenet.pkl"
+DATA_PKL_FILENAME_FEATURES_OUT = "01_75ms_feature_extract_out.pkl"
 
 
 # ------------------------------------------------------------ #
 # def main():
 #     pass
 
-# TODO
+# creating a training and test set
+df_train: pd.DataFrame = pd.read_pickle(
+    Path(DATA_INTERIM_PATH, DATA_PKL_FILENAME_FEATURES_OUT)
+)
+
+df_train.info()
+
+# dropping cols we do not need -- need to keep the ex col though
+df_train = df_train.drop(["participant", "set", "effort"], axis=1)
+
+# dropping labels for training set
+X = df_train.drop("ex", axis=1)
+# labels only
+y = df_train["ex"]
+
+# training/test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0
+)
 
 # ------------------------------------------------------------ #
 # for now we'll comment out because of periodic
